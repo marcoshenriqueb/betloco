cb0d775c5dd556cbb3ce6afc8aad59a7309c7597
marcos Almeida <marcoshenriqueb@gmail.com>
2016-05-26 16:09:48 -0300

    Changes market models

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 
 gulp.task('stylus', function () {
   gulp.src('./resources/stylus/app.styl')
     .pipe(stylus())
     .pipe(gulp.dest('./front/static/front/css'));
 });
-gulp.task('watch', ['stylus'], function() {
+
+gulp.task('script', function() {
+  browserify('./resources/js/app.js')
+  .bundle()
+  .pipe(source('app.bundled.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest('./front/static/front/js'));
+});
+
+gulp.task('watch', ['stylus', 'script'], function() {
   gulp.watch('./resources/stylus/**/*.styl', ['stylus']);
+  gulp.watch('./resources/js/**/*', ['script']);
 });
--- market/models.py
+++ market/models.py
@@ -13,21 +13,38 @@ class Market(models.Model):
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True,)
     market_type = models.ForeignKey(MarketType, on_delete=models.CASCADE,)
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False, blank=True)
-    deadline = models.DateTimeField(auto_now=False, auto_now_add=False, null=False, blank=False)
     trading_fee = models.DecimalField(max_digits=4, decimal_places=2)
     description = models.TextField()
+    deadline = models.DateTimeField(auto_now=False, auto_now_add=False, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False, blank=True)
 
 class Choice(models.Model):
     """docstring for Choice"""
     market = models.ForeignKey(Market, on_delete=models.CASCADE,)
     title = models.CharField(max_length=100)
 
-class Operation(models.Model):
-    """docstring for Operation"""
+class Order(models.Model):
+    """docstring for Order"""
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     choice = models.ForeignKey(Choice, on_delete=models.PROTECT)
     amount = models.IntegerField(blank=False, null=False)
     price = models.FloatField(validators = [MinValueValidator(0.0), MaxValueValidator(1.0)])
-    pending = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False, blank=True)
+    matches = models.ManyToManyField('self',
+                                       through='Operation',
+                                       through_fields=('from_order_id', 'to_order_id'),
+                                       symmetrical=False,
+                                       related_name='operation')
+
+class Operation(models.Model):
+    """docstring for Operation"""
+    from_order_id = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='from_order')
+    to_order_id = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='to_order')
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False, blank=True)
+    amount = models.PositiveIntegerField(blank=False, null=False)
+
+    class Meta:
+        unique_together = (('from_order_id', 'to_order_id'),)
