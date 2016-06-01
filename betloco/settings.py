345a0a8a4d34c2283da1d7d11bf9e16a4e34aec9
marcos Almeida <marcoshenriqueb@gmail.com>
2016-06-01 20:09:17 -0300

    adds static root

--- betloco/settings.py
+++ betloco/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/WWW/'
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
