0f5c9615bd229f867f221bcd33fe2d977071437d
marcos Almeida <marcoshenriqueb@gmail.com>
2016-07-01 19:09:09 -0300

    adds user social account serializer

--- user_profile/serializers.py
+++ user_profile/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from allauth.socialaccount.models import SocialAccount
+
+class SocialSerializer(serializers.ModelSerializer):
+    """docstring for UserSerializer"""
+    class Meta:
+        model = SocialAccount
+        fields = (
+            'extra_data',
+            'uid',
+            'provider'
+        )
 
 class UserSerializer(serializers.ModelSerializer):
     """docstring for UserSerializer"""
+    socialaccount_set = SocialSerializer(many=True)
     class Meta:
         model = User
         fields = (
@@ -11,5 +23,6 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'last_login',
-            'date_joined'
+            'date_joined',
+            'socialaccount_set'
         )
