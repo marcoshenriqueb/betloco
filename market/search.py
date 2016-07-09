272ba7ffd3a7234ec644544a4e3a92201d0f07fa
marcos Almeida <marcoshenriqueb@gmail.com>
2016-07-08 23:17:46 -0300

    min gram 4

--- market/search.py
+++ market/search.py
@@ -37,7 +37,7 @@ class ElasticSearch():
                           "filter": {
                             "autocomplete_filter": {
                                 "type": "edge_ngram",
-                                "min_gram": 1,
+                                "min_gram": 4,
                                 "max_gram": 20
                             }
                         },
@@ -83,51 +83,40 @@ class ElasticSearch():
 
     def search(self, query=None, pagination=10, page=0, expired=0, order="created_at|desc", category="todas"):
         try:
+            body = {
+                "sort" : [
+                    {order.split('|')[0] : {"order" : order.split('|')[1]}},
+                    { "_score": { "order": "desc" }}
+                ],
+                "from": page*pagination,
+                "size": pagination
+            }
             if query is None or len(query) == 0:
-                result = self.es.search(
-                    index="events-index",
-                    doc_type="events",
-                    body={
-                        "query":{
-                            "bool":{
-                                "must":{"match_all":{}},
-                            },
-                        },
-                        "sort" : [
-                            {order.split('|')[0] : {"order" : order.split('|')[1]}},
-                            { "_score": { "order": "desc" }}
-                        ],
-                        "from": page*pagination,
-                        "size": pagination
+                body['query'] = {
+                    "bool":{
+                        "must":{"match_all":{}},
                     }
-                )
+                }
             else:
-                result = self.es.search(
-                    index="events-index",
-                    doc_type="events",
-                    body={
-                        "query":{
-                            "bool":{
-                                "must":{
-                                    "multi_match": {
-                                      "type": "most_fields",
-                                      "query": query,
-                                      "fields": [
-                                            "title",
-                                            "title.folded"
-                                        ]
-                                    }
-                                }
-                            },
-                        },
-                        "sort" : [
-                            { "_score": { "order": "desc" }},
-                            {order.split('|')[0] : {"order" : order.split('|')[1]}}
-                        ],
-                        "from": page*pagination,
-                        "size": pagination
+                body['query'] = {
+                    "bool":{
+                        "must":{
+                            "multi_match": {
+                              "type": "most_fields",
+                              "query": query,
+                              "fields": [
+                                    "title",
+                                    "title.folded"
+                                ]
+                            }
+                        }
                     }
-                )
+                }
+            result = self.es.search(
+                index="events-index",
+                doc_type="events",
+                body=body
+            )
             if result['hits']['total'] <= (int(page)+1)*int(pagination):
                 result['next'] = None
             else:
