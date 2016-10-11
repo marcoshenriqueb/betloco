c5b51076eae35e087b17e1cb2fc9f434a0da4b14
marcos Almeida <marcoshenriqueb@gmail.com>
2016-10-11 13:38:03 -0300

    adds static urls

--- betloco/urls.py
+++ betloco/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
 from front.views import ChooseWinnerView, HomeView, LetsEncView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^custom-admin/choose-winner/$', ChooseWinnerView.as_view()),
@@ -15,4 +17,4 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^.well-known/acme-challenge/(?P<pk>.+)/$', LetsEncView.as_view()),
     url(r'^$', HomeView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
