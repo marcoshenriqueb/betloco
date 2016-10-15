36046ec40110072dad30844f1eca237e42b4b2d0
marcos Almeida <marcoshenriqueb@gmail.com>
2016-10-15 15:34:36 -0300

    starts what section

--- resources/js/home.js
+++ resources/js/home.js
@@ -1,36 +1,35 @@
 import theaterJS from "theaterjs";
 
 document.addEventListener("DOMContentLoaded", function(){
-  console.log(navigator.userAgent.toLowerCase().indexOf('firefox') > -1);
-  if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1){
-    var logo = document.querySelector(".logo").getBoundingClientRect();
-    var eyes = document.querySelectorAll(".logo_eye");
-    var height = document.documentElement.offsetHeight;
-    var width = document.documentElement.offsetWidth;
-    document.onmousemove = function(e){
-      if (e.pageY > logo.top && e.pageX > logo.left && e.pageY < logo.bottom && e.pageX < logo.right) {
-        eyes[0].style.transform = "translate(0,0)";
-        eyes[1].style.transform = "translate(0,0)";
-      }else {
-        var y = (e.pageY-((logo.top+logo.bottom)/2))/height;
-        var x = (e.pageX-((logo.left+logo.right)/2))/width;
-        var distY = e.pageY-((logo.top+logo.bottom)/2); //opposite
-        var distX = e.pageX-((logo.left+logo.right)/2); //adjacent
-        var dist = Math.sqrt((distY*distY)+(distX*distX)); //hypotenuse,
-        var val = distY/dist;
-        var aSine = Math.asin(val)*(180/Math.PI);
-        if (distX < 0) {
-          aSine = 180 - aSine;
-        }
-        aSine+=55;
-        var delta = 10;
-        eyes[0].style.transform = "translate("+x*delta+"px,"+y*delta+"px) rotate("+aSine+"deg)";
-        eyes[0].style.transformOrigin = "50% 50%";
-        eyes[1].style.transform = "translate("+x*delta+"px,"+y*delta+"px) rotate("+aSine+"deg)";
-        eyes[1].style.transformOrigin = "50% 50%";
-      }
-    }
-  }
+  // if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1){
+  //   var logo = document.querySelector(".logo").getBoundingClientRect();
+  //   var eyes = document.querySelectorAll(".logo_eye");
+  //   var height = document.documentElement.offsetHeight;
+  //   var width = document.documentElement.offsetWidth;
+  //   document.onmousemove = function(e){
+  //     if (e.pageY > logo.top && e.pageX > logo.left && e.pageY < logo.bottom && e.pageX < logo.right) {
+  //       eyes[0].style.transform = "translate(0,0)";
+  //       eyes[1].style.transform = "translate(0,0)";
+  //     }else {
+  //       var y = (e.pageY-((logo.top+logo.bottom)/2))/height;
+  //       var x = (e.pageX-((logo.left+logo.right)/2))/width;
+  //       var distY = e.pageY-((logo.top+logo.bottom)/2); //opposite
+  //       var distX = e.pageX-((logo.left+logo.right)/2); //adjacent
+  //       var dist = Math.sqrt((distY*distY)+(distX*distX)); //hypotenuse,
+  //       var val = distY/dist;
+  //       var aSine = Math.asin(val)*(180/Math.PI);
+  //       if (distX < 0) {
+  //         aSine = 180 - aSine;
+  //       }
+  //       aSine+=55;
+  //       var delta = 10;
+  //       eyes[0].style.transform = "translate("+x*delta+"px,"+y*delta+"px) rotate("+aSine+"deg)";
+  //       eyes[0].style.transformOrigin = "50% 50%";
+  //       eyes[1].style.transform = "translate("+x*delta+"px,"+y*delta+"px) rotate("+aSine+"deg)";
+  //       eyes[1].style.transformOrigin = "50% 50%";
+  //     }
+  //   }
+  // }
 
   var theater = theaterJS({
     "minSpeed": {
@@ -59,4 +58,8 @@ document.addEventListener("DOMContentLoaded", function(){
     .addScene('guroo:Prove o seu conhecimento.', 1200)
     .addScene('guroo:Ganhe com suas previsões.', 10000)
     .addScene(theater.replay)
+
+  $('.scrollspy').scrollSpy({
+    scrollOffset: 0
+  });
 });
