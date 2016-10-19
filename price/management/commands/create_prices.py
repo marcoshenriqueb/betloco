1674c0142e5ff8e68dc678ebb78af8d7356c63fd
marcos Almeida <marcoshenriqueb@gmail.com>
2016-10-19 15:11:11 -0200

    create_prices command2

--- price/management/commands/create_prices.py
+++ price/management/commands/create_prices.py
@@ -2,11 +2,13 @@ from django.core.management.base import BaseCommand, CommandError
 from market.models import Market
 from django.utils import timezone
 from datetime import timedelta
+from price.models import Price
 
 class Command(BaseCommand):
     help = 'Create table of historic prices'
 
     def handle(self, *args, **options):
+        Price.objects.all().delete()
         markets = Market.objects.all()
         for m in markets:
             if m.lastCompleteOrder is not None and m.liquidated == 0:
@@ -14,4 +16,8 @@ class Command(BaseCommand):
                     until = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=k)
                     o = m._getLastCompleteOrder(until=until)
                     if o is not None:
-                        print(o.price)
+                        Price.objects.create(
+                            market=m,
+                            date=until,
+                            price=o.price
+                        )
