2d43433de32d0255e9e58a2754d096f859bcbdd2
marcos Almeida <marcoshenriqueb@gmail.com>
2016-11-30 11:39:54 -0200

    es_refreshindex

---
+++ market/management/commands/es_refreshindex.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from market.search import ElasticSearch
+
+class Command(BaseCommand):
+    help = 'Indexes events model to ElasticSearch'
+
+    def handle(self, *args, **options):
+        e = ElasticSearch()
+        e.deleteEventIndex()
+        e.indexEvents()
